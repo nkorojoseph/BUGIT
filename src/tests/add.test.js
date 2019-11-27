f0568ce1a3756360974d6e2a78cd731e3ede015f
Aiico <aiico@FLR2-HQ-CAP-24.Aiicoplc.com>
2019-11-27 16:46:53 +0100

    package uprade

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -2,7 +2,7 @@ const add = (a, b) => a + b;
 const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`;
 
 test('should add two numbers', () => {
-  const result = add(3, 4);
+  const result = add(2,5);
   expect(result).toBe(7);
 });
 
